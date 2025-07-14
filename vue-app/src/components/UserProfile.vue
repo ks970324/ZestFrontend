124cff97be7e3b8b872d173d81ad9312066503d2
ks970324 <>
2025-07-14 18:05:58 +0800

    add email to user profile

--- vue-app/src/components/UserProfile.vue
+++ vue-app/src/components/UserProfile.vue
@@ -1,8 +1,11 @@
 <script setup>
 
 import { useAuthStore } from '../stores/auth'
-
 import { ref } from 'vue'
+
+
+const authStore = useAuthStore()
+const email = authStore.user?.email || 'Login to see your profile'
 
 const isProfileOpen = ref(false)
 
@@ -22,7 +25,7 @@ defineExpose({ openProfile })
   <div class="user-profile" v-if="isProfileOpen">
     <div class="user-profile-info">
       <div class="user-profile-img"></div>
-      <div class="user-profile-name"> {{ useAuthStore.user }}</div>
+      <div class="user-profile-name"> {{ email }}</div>
     </div>
     <button class="close-btn" @click="closeProfile">Close</button>
   </div>
@@ -33,16 +36,32 @@ defineExpose({ openProfile })
 <style scoped>
 
 .user-profile {
+  font-family: 'Press Start 2P', sans-serif;
   position: fixed;
   left: 40%;
   top: 20%;
-  width: 50%;
-  height: 50%;
-  background-color: rgba(0,0,0,0.5);
+  width: 30%;
+  height: 60%;
+  background-color: black;
   display: flex;
   align-items: center;
   justify-content: center;
   z-index: 1000;
+  color: #ffce3c;
 }
+
+.close-btn {
+  font-family: 'Press Start 2P', sans-serif;
+  font-size: 18px;
+  cursor: pointer;
+  color: #000000;
+  background-color: #ffce3c;
+  padding: 10px;
+}
+
+.close-btn:hover {
+  background-color: #ff9600;
+}
+
 
 </style>
