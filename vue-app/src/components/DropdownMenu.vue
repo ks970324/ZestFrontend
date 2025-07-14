9c9cd1263b4d86055d5b8fa26320138c749c90be
ks970324 <>
2025-07-15 02:32:20 +0800

    add dropdown menu in gamelobby page

---
+++ vue-app/src/components/DropdownMenu.vue
@@ -0,0 +1,83 @@
+<script setup>
+import UserProfile from '../components/UserProfile.vue';
+import AudioToggle from '../components/AudioToggle.vue';
+import { ref } from 'vue'
+
+const modalRef = ref()
+
+function openModal() {
+  modalRef.value?.openProfile()
+}
+
+
+</script>
+
+<template>
+  <div class="dropdown">
+    <!-- 主按鈕圖片 -->
+    <img src="/material/bag.png" alt="bag">
+
+    <!-- 下拉圖片選單 -->
+    <div class="dropdown-content">
+      <div class="dropdown-profile"  >
+          <img src="/material/profile.png" alt="profile" @click="openModal">
+      </div>
+      <div class="dropdown-music">
+        <AudioToggle AudioClass="audio-control" />
+      </div>
+    </div>
+    <UserProfile ref="modalRef" />
+  </div>
+</template>
+
+<style scoped>
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+  left:91%;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  top: 100%;
+  right: 5%;
+  background-color: #ab9377;
+  border: black solid 3px;
+}
+
+.dropdown-profile{
+  width: 100%;
+  border-bottom: black solid 3px;
+  box-sizing: border-box;
+  padding: 5px;
+  justify-content: center;
+}
+
+.audio-control{
+  width: 100%;
+  box-sizing: border-box;
+  padding: 5px;
+  justify-content: center;
+
+}
+
+.dropdown-music {
+  padding: 5px;
+}
+
+
+.dropdown-content img {
+  display: block;
+  width: 30px;
+  height:30px;
+  padding: 5px;
+  cursor: pointer;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+</style>
