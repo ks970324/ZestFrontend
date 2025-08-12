7684bf65239bfd7d56b035627b773e39932a2dbe
ks970324 <>
2025-08-12 10:48:16 +0800

    add cybershooter game page and router

---
+++ vue-app/src/pages/CyberShooter.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="cyber-shooter">
+
+  </div>
+</template>
+
+<style scoped>
+.cyber-shooter {
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: 'Press Start 2P', sans-serif;
+  background-color: #1e1e1e;
+  height: 100vh;
+  width: 100%;
+}
+</style>
--- vue-app/src/router.js
+++ vue-app/src/router.js
@@ -4,12 +4,14 @@ import Home from './pages/Home.vue'
 import Login from './pages/Login.vue'
 import GameLobby from './pages/GameLobby.vue'
 import Register from './pages/Register.vue'
+import CyberShooter from './pages/CyberShooter.vue'
 
 const routes = [
     { path: '/', name: 'Home',component: Home },
     { path: '/login', name: 'Login', component: Login },
     { path: '/gamelobby', name: 'GameLobby', component: GameLobby },
-    { path: '/register', name: 'Register', component: Register }
+    { path: '/register', name: 'Register', component: Register },
+    { path: '/cybershooter', name: 'CyberShooter', component: CyberShooter },
 ]
 
 const router = createRouter({
