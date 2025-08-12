0fa3a61cc70c334420dd6d2c7717a2dcd9fe7741
ks970324 <>
2025-08-12 10:43:48 +0800

    add form for user register data and character image choose in register page

---
+++ vue-app/src/components/CharacterSelect.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="character_title">
+    <span>Choose your Character</span>
+  </div>
+
+  <div class="character">
+    <div class="character_choose">
+      <!-- Gender -->
+      <div class="character_gender">
+        <div class="character_text_gender">Gender</div>
+        <div class="character_icon_gender">
+          <div
+              v-for="option in genders"
+              :key="option"
+              class="gender-icon"
+              :class="{ selected: character.gender === option }"
+              @click="character.gender = option"
+          >
+            <img :src="`material/gender-${option[0]}.png`" :alt="option" />
+          </div>
+        </div>
+      </div>
+
+      <!-- Hair -->
+      <div class="character_hair">
+        <div class="character_text_hair">Hair Color</div>
+        <div class="character_color_hair">
+          <div
+              v-for="color in hairs"
+              :key="color"
+              class="hair-color"
+              :class="{ selected: character.hair === color }"
+              @click="character.hair = color"
+          >
+            <img :src="`material/${color}color.png`" :alt="color" />
+          </div>
+        </div>
+      </div>
+
+      <!-- Clothes -->
+      <div class="character_cloth">
+        <div class="character_text_cloth">Clothes Color</div>
+        <div class="character_color_cloth">
+          <div
+              v-for="color in clothes"
+              :key="color"
+              class="clothes-color"
+              :class="{ selected: character.clothes === color }"
+              @click="character.clothes = color"
+          >
+            <img :src="`material/${color}color.png`" :alt="color" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- 角色圖片 -->
+    <div class="character_img">
+      <img :src="characterImage" alt="character" id="characterImg" />
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+import { reactive, computed,watch } from 'vue'
+import { useRegisterStore } from '../stores/register'
+
+const registerStore = useRegisterStore()
+
+// 狀態
+const character = reactive({
+  gender: 'boy',
+  hair: 'brown',
+  clothes: 'blue',
+})
+
+// 可選項目
+const genders = ['boy', 'girl']
+const hairs = ['black', 'orange', 'brown']
+const clothes = ['blue', 'darkgreen', 'gray']
+
+
+// 計算圖片來源
+const characterImage = computed(() => {
+  return `/material/${character.gender}_${character.hair}_${character.clothes}.png`
+})
+
+watch(character, () => {
+  registerStore.characterspath = characterImage.value
+  console.log('character path updated:', characterImage.value)
+}, { deep: true, immediate: true })
+
+</script>
+
+<style scoped>
+.character_title {
+  font-size: 22px;
+  text-align: center;
+  margin-bottom: 5%;
+}
+
+.character {
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  width: 100%;
+}
+
+.character_choose {
+  display: grid;
+  grid-auto-rows: 1fr;
+  row-gap: 5%;
+  margin-left: 5%;
+}
+
+.character_gender,
+.character_cloth,
+.character_hair {
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  height: 80px;
+}
+
+.character_icon_gender {
+  display: flex;
+  cursor: pointer;
+}
+
+.character_icon_gender img {
+  width: 40px;
+  margin-right: 30px;
+
+}
+
+
+.character_color_hair,
+.character_color_cloth {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 80%;
+  cursor: pointer;
+
+}
+
+.character_color_hair img,
+.character_color_cloth img {
+  width: 35%;
+  border: 2px solid rgba(132, 87, 54, 0.2);
+}
+
+.character_img {
+  display: grid;
+  place-items: center;
+  width: 100%;
+  margin-left:-30%;
+  margin-top: 20%;
+}
+
+.character_img img {
+  width: 80%;
+}
+
+</style>
--- vue-app/src/components/UserinfoForm.vue
+++ vue-app/src/components/UserinfoForm.vue
@@ -1,37 +1,162 @@
 <script setup>
 
+import {ref, watchEffect} from 'vue'
+import { checkemailapi } from "../services/checkemailapi.js";
+import { useRegisterStore} from "../stores/register.js";
+
+
+const email = ref('')
+const password = ref('')
+const confirmpassword = ref('')
+
+const registerStore = useRegisterStore()
+
+const emailError = ref(false)
+const passwordError = ref(false)
+
+const emaildirty = ref(false)
+const passworddirty = ref(false)
+const confirmpassworddirty = ref(false)
+
+const emailExists = ref()
+const checkpassword = ref()
+let emaildebounceTimer = null
+let passworddebounceTimer = null
+
+watchEffect(() => {
+  const newEmail = email.value
+
+  if (!emaildirty.value && newEmail) emaildirty.value = true
+  if (!emaildirty.value) return
+
+  if (!newEmail || newEmail.length < 5 || !newEmail.includes("@")) {
+    emailError.value = true;
+    return} else { emailError.value = false }
+
+  if (emaildebounceTimer) clearTimeout(emaildebounceTimer)
+  emaildebounceTimer = setTimeout(async () => {
+    try {
+      const response = await checkemailapi(newEmail)
+      emailExists.value = response.exists
+    } catch (err) {
+      console.error('Error checking email', err)
+    }
+  }, 300)
+
+  registerStore.email = newEmail
+
+})
+
+
+
+watchEffect(() => {
+  const newPassword = password.value
+  const newConfirmPassword = confirmpassword.value
+
+  if (!passworddirty.value && newPassword) passworddirty.value = true
+  if (!confirmpassworddirty.value && newConfirmPassword) confirmpassworddirty.value = true
+  if (!passworddirty.value && !confirmpassworddirty.value ) return
+
+  if (!newPassword || newPassword.length < 6) {
+    passwordError.value = true;
+    return
+  } else { passwordError.value = false }
+
+  if (passworddebounceTimer) clearTimeout(passworddebounceTimer)
+
+  passworddebounceTimer = setTimeout(() => {
+    checkpassword.value = newPassword === newConfirmPassword
+    if (!checkpassword.value) {
+      passwordError.value = false
+    }
+  }, 300)
+
+  registerStore.password = newPassword
+
+
+})
+
+
+
+
+
 </script>
 
 <template>
   <div class="title">
     <span class = "greeting_1">Welcome adventure! </span>
-    <p class = "greeting_2">Before you start your journey,</p>
-    <p class = "greeting_3">Please complete the required info.</p>
+    <span class = "greeting_2">Before you start your journey,</span>
+    <span class = "greeting_3">Please complete the required user information.</span>
   </div>
   <div class="userinfo">
     <label for="email">Email</label>
     <br>
-    <input type="text" id="email" required placeholder="">
+    <input
+        type="text"
+        id="email"
+        v-model="email"
+        required placeholder=""
+    />
+    <span v-if="emailExists === true">
+      <img src="/material/remove.png" alt="email" class="checkicon">
+    </span>
+    <span v-else-if="emailExists === false" style="color: red;">
+       <img src="/material/check.png" alt="email" class="checkicon">
+    </span>
     <br>
+    <div v-if="emailError" class="errormessage">
+      * Please make sure your email is valid.
+    </div>
     <label for="password">Password</label>
     <br>
-    <input type="text" id="password" required placeholder="">
+    <input
+        type="text"
+        id="password"
+        v-model="password"
+        required placeholder=""
+    />
     <br>
     <label for="confirm_password">Password Confirm</label>
     <br>
-    <input type="text" id="confirm_password" required placeholder="">
+    <input
+        type="text"
+        id="confirm_password"
+        required placeholder=""
+        v-model="confirmpassword">
+    <span v-if="checkpassword === false">
+      <img src="/material/remove.png" alt="email" class="checkicon">
+    </span>
+    <span v-else-if="checkpassword === true" >
+       <img src="/material/check.png" alt="email" class="checkicon">
+    </span>
+    <br>
+
+    <div v-if="passwordError" class="errormessage">
+      * Please make sure your password length must be at least 5 words.
+    </div>
+
   </div>
 </template>
 
 <style scoped>
 
+.title{
+  margin-top: 5%;
+  margin-bottom: 5%;
+  width: 98%;
+}
+
 .greeting_1{
-  font-size: 28px;
-  justify-content: center;
+  font-size: 26px;
+  text-align: center;
+  margin-bottom: 20px;
 }
 
 .greeting_2,  .greeting_3{
+  display: block;
   font-size: 14px;
+  line-height: 1.5;
+
 }
 
 
@@ -60,10 +185,15 @@ input:-webkit-autofill {
   transition: background-color 5000s ease-in-out 0s;
 }
 
-
-.userinfo input:valid {
-  color: rgba(132, 87, 54, 0.9);
+.checkicon{
+  width: 20px;
+  height: 20px;
 }
 
+.errormessage{
+  color: #F23C36;
+  font-size: 14px;
+  margin: 10px 0;
+}
 
 </style>
