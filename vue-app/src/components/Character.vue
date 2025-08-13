ad8e02b306819f48e95015672dd4525fee630103
ks970324 <>
2025-08-13 18:58:38 +0800

    add character in game lobby

---
+++ vue-app/src/components/Character.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="character">
+      <img src="/material/boy_brown_blue.png" alt="character" class="character-image">
+  </div>
+</template>
+
+<style scoped>
+
+</style>
