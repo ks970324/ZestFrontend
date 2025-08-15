4370b3e73d2029416e82ec269ac6346530467364
ks970324 <>
2025-08-15 15:55:57 +0800

    add login api in store

---
+++ vue-app/src/services/authapi.js
@@ -0,0 +1,10 @@
+// 📁 src/services/authApi.js
+import axios from 'axios'
+
+export async function loginApi(email, password, remember) {
+    return axios.post('http://localhost:5057/api/ZestAuth/Login', {
+        email,
+        password,
+        remember
+    })
+}
