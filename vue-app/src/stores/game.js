2703230cf4c29ac5ed1d17cddf7ff49a37e3740b
ks970324 <>
2025-08-28 18:08:55 +0800

    add game store to store variables

---
+++ vue-app/src/stores/game.js
@@ -0,0 +1,16 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+export const useGameStore = defineStore('game', {
+    state: () => ({
+        Playerhealth: 10,
+        Enemyhealth: 10,
+        Gameresult: '',
+    }),
+    actions: {
+        async gethealth() {
+            const response = await axios.get('http://localhost:5057/api/Game/gethealth')
+            this.health = response.data
+        },
+    }
+})
