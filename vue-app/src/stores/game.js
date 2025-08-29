ee524c6d2459695819c5d7860f97a6787e7555a8
ks970324 <>
2025-08-29 17:52:34 +0800

    add game round variables in pinia

--- vue-app/src/stores/game.js
+++ vue-app/src/stores/game.js
@@ -6,11 +6,14 @@ export const useGameStore = defineStore('game', {
         Playerhealth: 10,
         Enemyhealth: 10,
         Gameresult: '',
+        Currentround: '',
     }),
     actions: {
         async gethealth() {
             const response = await axios.get('http://localhost:5057/api/Game/gethealth')
             this.health = response.data
         },
+
+
     }
 })
