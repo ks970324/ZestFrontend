2b929ffc1b27cb4abfa5b14532858c3c16f6da59
ks970324 <>
2025-08-29 18:01:33 +0800

    add some component and adjust UI design in game page

--- vue-app/src/pages/CyberShooter.vue
+++ vue-app/src/pages/CyberShooter.vue
@@ -1,9 +1,35 @@
 <script setup>
 
+import Battlefield from "../components/Battlefield.vue";
+import HealthBar from "../components/HealthBar.vue";
+import GameRule from "../components/GameRule.vue";
+import GameAction from "../components/GameAction.vue";
+import {ref} from "vue";
+
+const gamerule = ref()
+
+function openGameRule() {
+  gamerule.value.checkRule()
+}
+
 </script>
 
 <template>
   <div class="cyber-shooter">
+    <div class="header">
+      <div class="exit">
+        <a href="/gamelobby"><img src="/material/door.png"></a>
+      </div>
+      <div class="question" @click="openGameRule()"><img src="/material/question-white.png"></div>
+    </div>
+    <div class="title"><h1>Cyber Shooter</h1></div>
+    <div class="battlefield">
+      <HealthBar />
+      <Battlefield />
+      <GameRule ref="gamerule"/>
+      <GameAction />
+
+    </div>
 
   </div>
 </template>
@@ -19,5 +45,25 @@
   background-color: #1e1e1e;
   height: 100vh;
   width: 100%;
+}
+
+.header {
+  display: flex;
+  width:98%;
+  padding:10px;
+  justify-content: space-between; /* 左右排列 */
+}
+
+.question, .exit{
+  cursor: pointer;
+}
+.exit img{
+  height:35px;
+}
+
+.title {
+  color: white;
+  font-size: 24px;
+  justify-content: center;
 }
 </style>
