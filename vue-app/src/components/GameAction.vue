8e067e8b8d7d53bdb1c6e07fa5ec6de82851d8de
ks970324 <>
2025-08-29 17:54:28 +0800

    add game action button UI

---
+++ vue-app/src/components/GameAction.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="buttons">
+    <div class="AttackButton" id="AttackButton">
+      <button>Attack</button>
+    </div>
+    <div class="MoveButton" id="MoveButton">
+      <button>Finish Move</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.buttons {
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+}
+
+.buttons button {
+  font-family: "Press Start 2P", sans-serif;
+  font-size: 16px;
+  padding: 10px;
+  background-color: #ffce3c;
+  cursor: pointer;
+  border: white 3px solid;
+  transition: top 0.1s ease;
+  position: relative;
+
+}
+
+.buttons button:hover {
+  background-color: #ff9600;
+}
+
+.buttons button:active {
+  top: 3px; /* 向下移動 3px */
+}
+
+#MoveButton, #AttackButton {
+}
+
+</style>
