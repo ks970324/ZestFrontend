7bfb194f9860213184b8991ed069a3973dd72efb
ks970324 <>
2025-09-03 17:44:40 +0800

    update state and action for move blue side character and both side attack

--- vue-app/src/stores/game.js
+++ vue-app/src/stores/game.js
@@ -1,17 +1,52 @@
 import { defineStore } from 'pinia'
-import axios from 'axios'
+import { updatebluepos } from "../services/updatebluepos.js";
+import { newgame } from "../services/newgame.js";
+import { attack } from "../services/attack.js";
 
 export const useGameStore = defineStore('game', {
     state: () => ({
-        Playerhealth: 10,
-        Enemyhealth: 10,
+        Bluehealth: 10,
+        Redhealth: 10,
         Gameresult: '',
-        Currentround: '',
+        CurrentRound: localStorage.getItem('CurrentRound'),
+        BluePosition: localStorage.getItem('BluePos')
+            ? JSON.parse(localStorage.getItem('BluePos'))
+            : { x: 1, y: 1 },
+        RedPosition: localStorage.getItem('RedPos')
+            ? JSON.parse(localStorage.getItem('RedPos'))
+            : { x: 1, y: 1 },
+        Hit:'',
     }),
     actions: {
-        async gethealth() {
-            const response = await axios.get('http://localhost:5057/api/Game/gethealth')
-            this.health = response.data
+        async updateblueposition() {
+            try {
+                const data = await updatebluepos(this.BluePosition, this.RedPosition)
+                this.BluePosition = data.bluePosition;
+                this.RedPosition = data.redPosition;
+                localStorage.setItem('BluePos', JSON.stringify(this.BluePosition))
+                localStorage.setItem('RedPos', JSON.stringify(this.RedPosition))
+                return data
+            } catch (error) {
+                console.error('updatebluepos failed', error)
+            }
+        },
+
+        async newgame() {
+            try{
+                const data = await newgame()
+                this.CurrentRound = data.currentRound
+                localStorage.setItem('CurrentRound', this.CurrentRound)
+                return data
+            } catch (error) {
+                console.error('newgame failed', error)
+            }
+
+        },
+
+        async attack() {
+            const data = await attack(this.BluePosition, this.RedPosition,this.CurrentRound)
+            this.Hit = data.hit
+            return data
         },
 
 
